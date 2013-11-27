23202b3730f9500f654455ba03f530ce8312d8d6
Mark Claydon <developer@glia.co>
2013-11-27 12:24:29 +0000

    Changed knife.rb

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -12,21 +12,28 @@ cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
 cookbook_path            ["#{current_dir}/../cookbooks"]
 
-knife[:role]                  = "cb-dev"
+knife[:role]                  = "cb-prod"
 
 # Ubuntu 12.04 LTS 64bit:
-knife[:image]                 = "ami-f968f8c9"
+# US 12.04
+#knife[:image]                 = "ami-f968f8c9"
+# EU 12.04
+knife[:image]                 = "ami-8fb1a3fb"
 knife[:use_sudo]              = "true"
 
 # Some defaults
-knife[:region]                = "us-west-2"
-knife[:availability_zone]     = "us-west-2c"
+#knife[:region]                = "us-west-2"
+#knife[:availability_zone]     = "us-west-2c"
+knife[:region]                = "eu-west-1"
+knife[:availability_zone]     = "eu-west-1b"
 knife[:ssh_user]              = "ubuntu"
-knife[:name]                  = "cb-dev"
+#knife[:name]                  = "cb-dev"
+knife[:name]                  = "cb-prod"
 knife[:flavor]                = "t1.micro"
 
 knife[:aws_access_key_id]  = "AKIAIYCGHZLABUBVBXRQ"
-knife[:aws_ssh_key_id] = 'cb-dev'
+#knife[:aws_ssh_key_id] = 'cb-dev'
+knife[:aws_ssh_key_id] = 'cb-prod'
 knife[:aws_secret_access_key] = "FGvbHUny2eSQKy5vIkoEmVYzHSspbk9tFET4PN6M"
 
 
